36e95288b533721708ab56ab61cf27d2d95df8d6
pvn <pedro.vicente@space-research.org>
2018-10-19 00:49:08 -0400

    add TCP server

--- examples/test_mapbox/test_mapbox.cc
+++ examples/test_mapbox/test_mapbox.cc
@@ -1,3 +1,20 @@
+#if defined (_MSC_VER)
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h> //hostent
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#endif
 #include <Wt/WApplication.h>
 #include <Wt/WText.h>
 #include <Wt/WCheckBox.h>
@@ -16,6 +33,156 @@
 #include "extensions/WLeafletMapbox.hh"
 using namespace Wt;
 
+unsigned short port = 2000;
+
+///////////////////////////////////////////////////////////////////////////////////////
+//tcp_server_t
+///////////////////////////////////////////////////////////////////////////////////////
+
+class tcp_server_t
+{
+public:
+  tcp_server_t() :
+    m_socket_fd(-1)
+  {
+#if defined (_MSC_VER)
+    WSADATA ws_data;
+    if (WSAStartup(MAKEWORD(2, 0), &ws_data) != 0)
+    {
+    }
+#endif
+  };
+  ~tcp_server_t()
+  {
+#if defined (_MSC_VER)
+    WSACleanup();
+#endif
+  };
+  int listen(const unsigned short server_port);
+  int accept_client();
+  int read_all(int socket_client_fd, void *_buf, int size_buf);
+  void close_socket()
+  {
+#if defined (_MSC_VER)
+    closesocket(m_socket_fd);
+#else
+    close(m_socket_fd);
+#endif
+  }
+private:
+  int m_socket_fd; // socket descriptor 
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+//tcp_server_t::listen
+///////////////////////////////////////////////////////////////////////////////////////
+
+int tcp_server_t::listen(const unsigned short server_port)
+{
+  // local address
+  sockaddr_in server_addr;
+
+  // create TCP socket for incoming connections
+  if ((m_socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
+    std::cout << "socket error: " << std::endl;
+    return -1;
+  }
+
+  // construct local address structure
+  memset(&server_addr, 0, sizeof(server_addr));     // zero out structure
+  server_addr.sin_family = AF_INET;                 // internet address family
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // any incoming interface
+  server_addr.sin_port = htons(server_port);        // local port
+
+   // bind to the local address
+  if (::bind(m_socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+  {
+    //bind error: Permission denied
+    //probably trying to bind a port under 1024. These ports usually require root privileges to be bound.
+    std::cout << "bind error: " << std::endl;
+    return -1;
+  }
+
+  // mark the socket so it will listen for incoming connections
+  if (::listen(m_socket_fd, 10) < 0)
+  {
+    std::cout << "listen error: " << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//tcp_server_t::accept_client
+///////////////////////////////////////////////////////////////////////////////////////
+
+int tcp_server_t::accept_client()
+{
+  int socket_client_fd; // socket descriptor for client
+  sockaddr_in addr_client; // client address
+#if defined (_MSC_VER)
+  int len_addr; // length of client address data structure
+#else
+  socklen_t len_addr;
+#endif
+
+  // set length of client address structure (in-out parameter)
+  len_addr = sizeof(addr_client);
+
+  // wait for a client to connect
+  if ((socket_client_fd = ::accept(m_socket_fd, (struct sockaddr *) &addr_client, &len_addr)) < 0)
+  {
+    std::cout << "accept error " << std::endl;
+    return -1;
+  }
+
+  return socket_client_fd;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//tcp_server_t::read_all
+//read SIZE_BUF bytes of data from SOCKET_CLIENT_FD into buffer BUF 
+//return total size read
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int tcp_server_t::read_all(int socket_client_fd, void *_buf, int size_buf)
+{
+  char *buf = static_cast<char *>(_buf); // can't do pointer arithmetic on void* 
+  int recv_size; // size in bytes received or -1 on error 
+  size_t size_left; // size in bytes left to send 
+  const int flags = 0;
+  int total_recv_size = 0;
+
+  size_left = size_buf;
+
+  while (size_left > 0)
+  {
+    //read the data, being careful of interrupted system calls and partial results
+    do
+    {
+      recv_size = recv(socket_client_fd, buf, size_left, flags);
+    } while (-1 == recv_size && EINTR == errno);
+
+    if (-1 == recv_size)
+    {
+      std::cout << "recv error: " << strerror(errno) << std::endl;
+    }
+
+    //everything received, exit
+    if (0 == recv_size)
+    {
+      break;
+    }
+
+    size_left -= recv_size;
+    buf += recv_size;
+    total_recv_size += recv_size;
+  }
+
+  return total_recv_size;
+}
+
 //run
 //./leaflet_mapbox.wt --http-address=0.0.0.0 --http-port=8080  --docroot=. -c wt_config.xml
 
@@ -32,22 +199,24 @@ class MapApplication : public WApplication
 public:
   MapApplication(const WEnvironment& env) :
     WApplication(env),
-    m_iter(300)
+    m_value(300)
   {
     m_hbox = root()->setLayout(cpp14::make_unique<WVBoxLayout>());
-    m_text = m_hbox->addWidget(cpp14::make_unique<WText>(Wt::asString(m_iter)));
+    m_text = m_hbox->addWidget(cpp14::make_unique<WText>(Wt::asString(m_value)));
     m_leaflet = m_hbox->addWidget(cpp14::make_unique<WMap>(tile_provider_t::CARTODB, 38.9072, -77.0369, 13));
-    m_leaflet->Circle(38.9072, -77.0369, m_iter, "#ff0000");
+    m_leaflet->Circle(38.9072, -77.0369, m_value, "#ff0000");
 
     enableUpdates(true);
     if (m_listen_thread.joinable())
     {
       m_listen_thread.join();
     }
+    m_server.listen(port);
     m_listen_thread = std::thread(std::bind(&MapApplication::listen, this));
   }
   virtual ~MapApplication()
   {
+    m_server.close_socket();
     if (m_listen_thread.get_id() != std::this_thread::get_id() && m_listen_thread.joinable())
     {
       m_listen_thread.join();
@@ -58,22 +227,30 @@ private:
   WVBoxLayout *m_hbox;
   WText *m_text;
   WMap *m_leaflet;
-  size_t m_iter;
+  //value received in listening socket
+  int m_value;
   std::thread m_listen_thread;
+  tcp_server_t m_server;
 
   void listen()
   {
     while (true)
     {
-      std::this_thread::sleep_for(std::chrono::seconds(7));
+      char buf[255];
+      int socket_client_fd = m_server.accept_client();
+      int recv_size = m_server.read_all(socket_client_fd, buf, sizeof(buf));
+      std::string str(buf);
+      str.resize(recv_size);
+      m_value = std::stoi(str);
+      std::this_thread::sleep_for(std::chrono::seconds(5));
+
       WApplication::UpdateLock uiLock(this);
       if (uiLock)
       {
-        m_iter += 100;
-        m_text->setText(Wt::asString(m_iter));
+        m_text->setText(Wt::asString(m_value));
         m_leaflet->removeFromParent();
         m_leaflet = m_hbox->addWidget(cpp14::make_unique<WMap>(tile_provider_t::CARTODB, 38.9072, -77.0369, 13));
-        m_leaflet->Circle(38.9072, -77.0369, m_iter, "#ff0000");
+        m_leaflet->Circle(38.9072, -77.0369, m_value * 20, "#ff0000");
         triggerUpdate();
       }
       else
